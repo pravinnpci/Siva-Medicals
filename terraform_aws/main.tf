19ea88c064e0a930809013cfeb56a2af460451f8
localadmin <sapravin46@gmail.com>
2026-04-15 11:28:12 +0530

    docker host

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -43,8 +43,8 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${var.aws_region}a"
   tags = {
